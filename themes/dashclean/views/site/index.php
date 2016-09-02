7c7b398d8480fa1f237eeb87e4c95d75346d6790
felixtaximo <felix.vargas@taximo.co>
2016-09-02 11:56:17 -0500

    se muestra icono de avl

--- themes/dashclean/views/site/index.php
+++ themes/dashclean/views/site/index.php
@@ -27,7 +27,7 @@
             </div>
             <div class="box-content">
                 <div class="btn-group-box">
-<!--                    <button class="btn" onclick="location.href='<?php //echo Yii::app()->baseUrl; ?>/avl/searchVehicle'"><i class="icon-dashboard icon-large"></i><br/>Mis vehículos</button>-->
+                    <button class="btn" onclick="location.href='<?php echo Yii::app()->baseUrl; ?>/avl/searchVehicle'"><i class="icon-dashboard icon-large"></i><br/>Mis vehículos</button>
                     <button class="btn" onclick="location.href='<?php echo Yii::app()->baseUrl; ?>/charts'"><i class="icon-dashboard icon-large"></i> Telemedición</button>
                 </div>
             </div>
